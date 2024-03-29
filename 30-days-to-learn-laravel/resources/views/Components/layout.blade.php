4f0eb3dfad850bca047f438d87d0f91f46312a48
Udayanga <udayanga.wijesiri@gmail.com>
2024-03-29 16:52:45 +0530

    30-days-to-learn-laravel: style active nav link

--- 30-days-to-learn-laravel/resources/views/Components/layout.blade.php
+++ 30-days-to-learn-laravel/resources/views/Components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,7 @@
   <script src="https://cdn.tailwindcss.com"></script>
   <title>Document</title>
 </head>
-<body>
+<body class="h-full">
   {{-- <nav>
     <x-nav-link href="/">Home</x-nav-link>
     <x-nav-link href="/about">About</x-nav-link>
@@ -34,9 +34,9 @@
           <div>
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <x-nav-link href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</x-nav-link>
-              <x-nav-link href="/about" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</x-nav-link>
-              <x-nav-link href="/contact" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Contact</x-nav-link>
+              <x-nav-link href="/" :active="request()->is('/')" aria-current="page">Home</x-nav-link>
+              <x-nav-link href="/about" :active="request()->is('about')" aria-current="page">About</x-nav-link>
+              <x-nav-link href="/contact" :active="request()->is('contact')" aria-current="page">Contact</x-nav-link>
             </div>
           </div>
         </div>
--- 30-days-to-learn-laravel/resources/views/Components/nav-link.blade.php
+++ 30-days-to-learn-laravel/resources/views/Components/nav-link.blade.php
@@ -1,1 +1,2 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+@props(['active' => false]) // default is false
+<a class="{{ $active ? 'bg-gray-900 text-white rounded-md px-3 py-2' : 'bg-gray-700 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2' }}" {{ $attributes }}>{{ $slot }}</a>
